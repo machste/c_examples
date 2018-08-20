2a357c6caf1e30afdad44983efa5ade8966b35e2
Stefan Mächler <stefan.maechler@neratec.com>
2018-08-20 12:23:45 +0200

    Fix argument parsing for encode64

--- base64/encode64.c
+++ base64/encode64.c
@@ -6,17 +6,18 @@
 
 int main(int argc, char *argv[])
 {
-    size_t in_len = 0;
-    size_t argv_len[argc];
     // Analyse arguments
+    size_t in_size = 0;
+    size_t argv_len[argc];
     for (int i = 1; i < argc; i++) {
         size_t arg_len = strlen(argv[i]);
         argv_len[i] = arg_len;
-        in_len += arg_len;
+        // Add argument length puls one for either a space or null-termination
+        in_size += (arg_len + 1);
     }
     // Concatinate all in arguments
     size_t in_pos = 0;
-    char *in = malloc(in_len + argc);
+    char *in = malloc(in_size);
     for (int i = 1; i < argc; i++) {
         memcpy(in + in_pos, argv[i], argv_len[i]);
         in_pos += argv_len[i];
@@ -25,10 +26,10 @@ int main(int argc, char *argv[])
     }
     in[in_pos] = '\0';
     // Prepare Output
-    size_t out_size = (in_len / 3 + 1) * 4 + 1;
+    size_t out_size = (in_pos / 3 + 1) * 4 + 1;
     char *out = malloc(out_size);
     // Encode base64
-    if (base64_encode(in, in_len, out, out_size) > 0) {
+    if (base64_encode(in, in_pos, out, out_size) > 0) {
         // Write out to standard out
         puts(out);
     } else {
